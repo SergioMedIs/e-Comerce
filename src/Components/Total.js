8975ec03e4568f30f261c9bdc068e8b6547176e9
sergioMeIs <semedina.dwfc@ucamp.utel.edu.mx>
2022-09-04 15:24:23 -0500

    reuploadv56

---
+++ src/Components/Total.js
@@ -0,0 +1,31 @@
+import React from "react";
+import accounting from "accounting";
+import { Button } from "@mui/material";
+import {Link }from 'react-router-dom'
+import {getBasketTotal} from './../reducer'
+import { useStateValue}   from '../StateProvider';
+
+
+
+export default function Total() {
+    
+    const [{basket}]=useStateValue();
+    return (
+        <div>
+
+<h5>TOTAL :</h5>  
+{accounting.formatMoney(getBasketTotal(basket),"$")}
+           
+       
+
+<Link style={{textDecoration:"none", color:"white"}} to={'/'}>
+<Button size="small">Continuar</Button>
+     </Link>
+        </div>
+        
+
+    )
+}
+
+
+   
